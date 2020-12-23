eab22c92587310955def0821663eeedb6758df2c
Alexandra Spalato <alexaspalato@gmail.com>
2020-12-23 16:43:18 +0100

    working

---
+++ packages/gatsby-theme-test/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#10b3b1",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
