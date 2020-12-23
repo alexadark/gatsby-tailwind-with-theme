d0eb9ef563b1e4a0ddf5eb69552984d199130c73
Alexandra Spalato <alexaspalato@gmail.com>
2020-12-23 18:10:30 +0100

    working in both

---
+++ site/src/pages/test.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const TestPage = () => {
+  return (
+    <div>
+      <h1 className="p-6 bg-primary text-secondary">test</h1>
+    </div>
+  )
+}
+
+export default TestPage
--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       colors: {
         primary: "#10b3b1",
+        secondary: "tomato",
       },
     },
   },
